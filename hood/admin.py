54bbfe5476662db7c80673e24ba493a4869b7ed0
sharon002 <sharonjep2016@gmail.com>
2021-07-26 12:19:45 +0300

    add admin

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,11 @@
-from django.contrib import admin
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 
+from django.contrib import admin
+from .models import neighbourhood, Profile, Authorities, Health, Post
 # Register your models here.
+admin.site.register(neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Authorities)
+admin.site.register(Health)
+admin.site.register(Post)
