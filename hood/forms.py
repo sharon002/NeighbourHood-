45493417bf9a8e4938475c13a53c543efb694c35
sharon002 <sharonjep2016@gmail.com>
2021-07-26 12:21:30 +0300

    add forms file

---
+++ hood/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import Profile, Post
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+
+
+class RegisterForm(UserCreationForm):
+    email= forms.EmailField()
+    
+    class Meta:
+        model= User
+        fields= ['username','email','password1', 'password2']
+        
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model=Profile
+        exclude=['username']
+        
+class PostForm(forms.ModelForm):
+    class Meta:
+        model=Post
+        exclude=['username','neighbourhood','avatar']
